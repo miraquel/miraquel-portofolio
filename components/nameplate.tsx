82e0282cad0945bb57d241e369cc7a3993b0bff4
Chaidir Ali Assegaf <cassegaf@intikom.co.id>
2022-04-23 20:28:23 +0700

    Mobile Layout Fix

--- components/nameplate.tsx
+++ components/nameplate.tsx
@@ -6,12 +6,12 @@ const NamePlate: React.FC<{className?: string}> = ({className}) => {
         <div className={`${className} px-8 pt-8 pb-6 font-bold z-10 text-white
         flex flex-col justify-center items-center flex-1
         border-2 border-white rounded-lg transition-all duration-500`}>
-            <h1 className="mb-6 text-4xl xl:tracking-widest">Chaidir Ali Assegaf</h1>
-            <h2 className="mb-6 text-2xl xl:tracking-wide tracking-tight ">
+            <h1 className="mb-6 xl:text-4xl md:text-2xl text-xl xl:tracking-widest">Chaidir Ali Assegaf</h1>
+            <h2 className="mb-6 xl:text-2xl md:text-xl text-md xl:tracking-wide tracking-tight ">
                 <span>Software Developer</span>
             </h2>
-            <div className="text-4xl flex">
-                <SocialLinks className="px-7 hover:scale-[135%] duration-[.25] transition-all" />
+            <div className="xl:text-4xl md:text-2xl text-xl flex">
+                <SocialLinks className="xl:px-7 md:px-4 px-2 hover:scale-[135%] duration-[.25] transition-all" />
             </div>
         </div>
     )
